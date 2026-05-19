9374927d87e2b419a0165446b0161afaeff37b36
pira1508 <pira1508@gmail.com>
2026-05-18 19:34:08 -0500

    analytics: instalar TikTok Pixel global (carga en / y /v2)
    Pixel ID D85QV8BC77U8MI6UL30G. Se carga via next/script con strategy
    afterInteractive, dispara ttq.page() en cada navegación.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -15,6 +15,7 @@ export const metadata: Metadata = {
 };
 
 const META_PIXEL_ID = "722614530760265";
+const TIKTOK_PIXEL_ID = "D85QV8BC77U8MI6UL30G";
 const GA4_ID = process.env.NEXT_PUBLIC_GA4_ID || "";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
@@ -29,6 +30,15 @@ export default function RootLayout({ children }: { children: React.ReactNode }) 
           document,'script','https://connect.facebook.net/en_US/fbevents.js');
           fbq('init', '${META_PIXEL_ID}');
           fbq('track', 'PageView');
+        `}</Script>
+        <Script id="tiktok-pixel" strategy="afterInteractive">{`
+          !function (w, d, t) {
+            w.TiktokAnalyticsObject=t;var ttq=w[t]=w[t]||[];ttq.methods=["page","track","identify","instances","debug","on","off","once","ready","alias","group","enableCookie","disableCookie","holdConsent","revokeConsent","grantConsent"],ttq.setAndDefer=function(t,e){t[e]=function(){t.push([e].concat(Array.prototype.slice.call(arguments,0)))}};for(var i=0;i<ttq.methods.length;i++)ttq.setAndDefer(ttq,ttq.methods[i]);ttq.instance=function(t){for(
+            var e=ttq._i[t]||[],n=0;n<ttq.methods.length;n++)ttq.setAndDefer(e,ttq.methods[n]);return e},ttq.load=function(e,n){var r="https://analytics.tiktok.com/i18n/pixel/events.js",o=n&&n.partner;ttq._i=ttq._i||{},ttq._i[e]=[],ttq._i[e]._u=r,ttq._t=ttq._t||{},ttq._t[e]=+new Date,ttq._o=ttq._o||{},ttq._o[e]=n||{};n=document.createElement("script")
+            ;n.type="text/javascript",n.async=!0,n.src=r+"?sdkid="+e+"&lib="+t;e=document.getElementsByTagName("script")[0];e.parentNode.insertBefore(n,e)};
+            ttq.load('${TIKTOK_PIXEL_ID}');
+            ttq.page();
+          }(window, document, 'ttq');
         `}</Script>
         {GA4_ID && (
           <>
